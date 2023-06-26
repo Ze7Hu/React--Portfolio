ff5ad6959d439ad6fcd11dd61005ee04bbc7acba
Hodan Ibrahim <hodanibrahim@hodans-air.home>
2023-06-26 16:52:57 +0100

    changes for deployment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 
-
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
